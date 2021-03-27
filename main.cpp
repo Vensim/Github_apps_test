10f5127d9eb5ca9dc1323796a4b08f10e41dc931
Venmis <nmisiukonis430@gmail.com>
2021-03-27 22:04:48 +0000

    assertion success test

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
 	assert(2 == sum(1, 1));
 	assert(c == sum(a, b));
 	assert(0 == sum(0, 0));
-	assert(0 == sum(0,1));
+	assert(1 == sum(0,1));
 	// Other test cases
 std::cout << "Test" << std::endl;
 
