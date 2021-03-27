fa79f88083e6c77ca1fe43910c439c5f6547831f
Venmis <nmisiukonis430@gmail.com>
2021-03-27 21:50:56 +0000

    Assert.h test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <assert.h>
+
+int sum(int a, int b ) {
+	return a + b;
+}
+
 
 int main() {
-int a = 10;
-int b = 5;
+
+	int a = 10;
+	int b = 5;
+	int c = a + b;
+	assert(2 == sum(1, 1));
+	assert(c == sum(a, b));
+	assert(0 == sum(0, 0));
+
+	// Other test cases
 std::cout << "Test" << std::endl;
 
 }
